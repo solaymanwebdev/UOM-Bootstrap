b007406b803be383fc89166a11c5946ea9ef13a1
Solayman Shahid <solaymanshahid67@gmail.com>
2026-08-06 09:59:24 +0600

    add something

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-$('.bannerSlider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 1000,
-    prevArrow: '<i class="fa-solid fa-angle-left prev"></i>',
-    nextArrow: '<i class="fa-solid fa-angle-right next"></i>',
-    dots: true,
+
+$('.bannerSlide').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  prevArrow:'<i class="fa-solid fa-angles-left prev"></i>',
+  nextArrow:'<i class="fa-solid fa-angles-right next"></i>',
 });
 
 (function () {
@@ -51,3 +51,8 @@ $('.bannerSlider').slick({
         //seconds
       }, 0)
   }());
+
+
+  var containerEl = document.querySelector('.filter');
+
+            var mixer = mixitup(containerEl);
